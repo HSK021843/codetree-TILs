ca09d754c09e21b19b4115a4690f2f88eb3c532c
HSK021843 <135344588+HSK021843@users.noreply.github.com>
2024-02-22 23:40:26 +0900

    Title: 함수를 이용한 윤년 판별, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240222/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240222/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,8 +1,9 @@
 def find_year(year):
-    if year % 4 == 0 or (year % 100 == 0 and year % 400 != 0):
-        print("true")
-    else:
-        print("false")
+    if year % 4 == 0:
+        if (year % 100 == 0 and year % 400 != 0):
+            print("false")
+        else:
+            print("true")
 
 y = int(input())
 res = find_year(y)
