b7f8dd054d197f6727e35e11fbdffb3a600f9178
HSK021843 <135344588+HSK021843@users.noreply.github.com>
2026-06-12 20:21:36 +0900

    [Passed] 피보나치 수 (49ms, 16MB)

---
+++ trail4/피보나치 수/fibonacci-number.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+# Please write your code here.
+if N == 1 or N == 2:
+    print(1)
+else:
+    dp = [0 for _ in range(N + 1)]
+    dp[1] = 1
+    dp[2] = 1
+
+
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    print(dp[-1])
